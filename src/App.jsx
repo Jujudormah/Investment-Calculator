f9ae9250289de2c1d76b8bf6b0da11ce4f2e371b
Jujudormah <gustavo_diaz14@yahoo.com>
2026-04-08 16:32:19 -0700

    Header and img creation for project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import Header from "../components/header.jsx";
+
 function App() {
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <div>
+      <Header />
+    </div>
+  );
 }
 
-export default App
+export default App;

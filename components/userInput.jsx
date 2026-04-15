e0f9303b856be58fe199931ed03f5e12f6a2c21d
Jujudormah <gustavo_diaz14@yahoo.com>
2026-04-14 18:57:02 -0700

    Form/section setup for input calculations along with changing flex to grid in css

---
+++ components/userInput.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+
+export default function UserInput() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 5,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  }
+
+  return (
+    <section id="user-input">
+      <div className="input-group">
+        <div>
+          <label>Initial Investment</label>
+          <input
+            type="number"
+            value={userInput.initialInvestment}
+            onChange={(e) => handleChange("initialInvestment", e.target.value)}
+          />
+        </div>
+      </div>
+      <div className="input-group">
+        <div>
+          <label>Expected Return</label>
+          <input
+            type="number"
+            value={userInput.expectedReturn}
+            onChange={(e) => handleChange("expectedReturn", e.target.value)}
+          />
+        </div>
+      </div>
+      <div className="input-group">
+        <div>
+          <label>Annual Investment</label>
+          <input
+            type="number"
+            value={userInput.annualInvestment}
+            onChange={(e) => handleChange("annualInvestment", e.target.value)}
+          />
+        </div>
+      </div>
+      <div className="input-group">
+        <div>
+          <label>Duration (years)</label>
+          <input
+            type="number"
+            value={userInput.duration}
+            onChange={(e) => handleChange("duration", e.target.value)}
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
